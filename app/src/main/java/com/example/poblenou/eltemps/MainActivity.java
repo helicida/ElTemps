962cd31e3e2743494b4e670cd3ec0a7a7109afa9
Sergi <sergibca@gmail.com>
2015-10-20 19:05:28 +0200

    Afegits comentaris.

--- app/src/main/java/com/example/poblenou/eltemps/MainActivity.java
+++ app/src/main/java/com/example/poblenou/eltemps/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);     //Referenciem per la ID
         setSupportActionBar(toolbar);
 
         //ActionBar
